0cd9aebf0a913bf6ba942c5ef8843933b9eb229b
VuDucTN <89180923+VuDucTN@users.noreply.github.com>
2023-03-07 19:35:14 +0700

    1
    fist

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://drive.google.com/drive/u/0/folders/128975YWaAjX1ZyBxNAdh1Sfkp3vrgJHt';
+const eventName = 'Chúc Em 8-3';
+const titleCard = 'Tặng Kim Huyền';
+const contentCard = 'Chúc người yêu của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'em.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
